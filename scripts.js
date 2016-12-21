c52cf8e6ee4a2d2e4a7de380d9ccd954c24c9e39
Matt Bedell <matt@bedell.co>
2016-12-21 13:20:23 -0500

    add component and controller classes

--- scripts.js
+++ scripts.js
@@ -1,11 +1,39 @@
+class UIController {
+  constructor(componentsToRegister) {
+    this.components = {}
+    this.registerComponents(componentsToRegister)
+    this.registerMouseInOutHandlers(this.components)
+  }
+  registerComponents(componentsObj) {
+    // save project components
+    this.components.projects = componentsObj.projects
+  }
+  registerMouseInOutHandlers(components) {
+    for(let key in components) {
+      console.log(key)
+    }
+  }
+}
 class ProjectComponent {
   constructor(elmnt) {
-    this.elmnt = $(elmnt);
+    this.type = 'project'
+    this.elmnt = $(`.${elmnt}`);
+    this.visible = true; // ***** set to false in production
   }
-  
+  toggleFade() {
+    this.elmnt.toggleClass('project-fade')
+  }
 }
+const projectPacman = new ProjectComponent('project-pacman');
+const projectAsteroid = new ProjectComponent('project-asteroid-tracker')
+let components = {
+  projects: {
+    projectPacman,
+    projectAsteroid
+  }
+}
+const controller = new UIController(components)
 $(() => {
   $('.title-fade').removeClass('title-fade');
   $('.navBox').removeClass('navBox-fade');
-
 })
