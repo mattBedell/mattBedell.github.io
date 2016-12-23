8fce7ceb2b8265f69e1f9a56f9258670c1b960c1
Matt Bedell <matt@bedell.co>
2016-12-23 05:07:44 -0500

    add set component visibility to hidden when faded out

--- scripts.js
+++ scripts.js
@@ -8,14 +8,16 @@ class UIController {
   // Updates controller to currently selected navButton, hides all other content
   focusSelect(navComponent) {
     let fadeCounter = 5;
-    this.components.content.forEach((item) => {
-      if(item.type === navComponent.name) {
-        fadeCounter++;
-        item.fadeIn(1, fadeCounter/20);
-      } else if(item.type === this.navSelect) {
-        item.fadeOut(.5, 0);
-      }
+    // hide curently selected content
+    this.components.content[this.navSelect].forEach((item) => {
+      item.fadeOut(.5, 0)
     })
+    // reveal newly selected content
+    this.components.content[navComponent.name].forEach((item) => {
+      fadeCounter++
+      item.fadeIn(1, fadeCounter/20)
+    })
+    // inform controller of currently selected content
     this.navSelect = navComponent.name
   }
   // Add click events to nav buttons
@@ -27,35 +29,51 @@ class UIController {
     })
   }
 }
-class Component {
+class ContentComponent {
   constructor(elmnt) {
     this.name = elmnt.split('-')[1]
     this.type = elmnt.split('-')[0]
     this.elmnt = $(`.${elmnt}`);
     this.visible = true; // ***** set to false in production
+    this.elmnt.on('transitionend', () => {
+      if(!this.visible) {
+        this.elmnt.css('visibility', 'hidden')
+      }
+    })
   }
   fadeIn(speed, delay) {
-    this.elmnt.css({'transition': `${speed}s ${delay}s`, 'opacity': 1})
-
-    //this.elmnt.removeClass('project-fade')
+    this.visible = true;
+    this.elmnt.css({'visibility': 'visible', 'transition': `${speed}s ${delay}s`, 'opacity': 1})
   }
   fadeOut(speed, delay) {
+    this.visible = false;
     this.elmnt.css({'transition': `${speed}s ${delay}s`,'opacity': 0})
-    //this.elmnt.addClass('project-fade')
   }
 }
-const pacman = new Component('project-pacman');
-const asteroid = new Component('project-asteroid-tracker')
-const blink = new Component('project-blink')
-const twitterSearch = new Component('project-twitterSearch')
-const about = new Component('nav-about')
-const projects = new Component('nav-project')
-const contact = new Component('nav-contact')
-const aboutMain = new Component('about-main')
-const aboutAside = new Component('about-aside')
+class NavComponent {
+  constructor(elmnt) {
+    this.name = elmnt.split('-')[1]
+    this.type = elmnt.split('-')[0]
+    this.elmnt = $(`.${elmnt}`);
+  }
+}
+const pacman = new ContentComponent('project-pacman');
+const asteroid = new ContentComponent('project-asteroid-tracker')
+const blink = new ContentComponent('project-blink')
+const twitterSearch = new ContentComponent('project-twitterSearch')
+const aboutMain = new ContentComponent('about-main')
+const aboutAside = new ContentComponent('about-aside')
+
+const about = new NavComponent('nav-about')
+const projects = new NavComponent('nav-project')
+const contact = new NavComponent('nav-contact')
 
 let components = {
-  content: [pacman, asteroid, blink, twitterSearch, aboutMain, aboutAside],
+  content: {
+    project: [pacman, asteroid, blink, twitterSearch],
+    about: [aboutMain, aboutAside],
+    contact: []
+  },
   nav: [about, projects, contact]
 }
 const controller = new UIController(components)
