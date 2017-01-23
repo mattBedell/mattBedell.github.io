d4bff087458fa3de3b0f0cb1a69cc1689191b0d5
Matt Bedell <matt@bedell.co>
2017-01-23 17:56:16 -0500

    add email sending status

--- portfolio/src/components/Contact/Contact.jsx
+++ portfolio/src/components/Contact/Contact.jsx
@@ -9,7 +9,8 @@ class Contact extends Component {
       name: '',
       email: '',
       text: '',
-      componentOpacity: 0
+      componentOpacity: 0,
+      status: 'not sent'
     }
   }
   updateField(inputField, e) {
@@ -18,6 +19,9 @@ class Contact extends Component {
     })
   }
   submitForm() {
+    this.setState({
+      status: 'sending'
+    })
     const {name, email, text} = this.state;
     const payload = {
       name,
@@ -30,6 +34,13 @@ class Contact extends Component {
       },
       method: 'POST',
       body: JSON.stringify(payload)
+    }).then((res) => {
+      if(res.ok) {
+        console.log('response ok');
+        this.setState({
+          status: 'sent'
+        })
+      }
     })
   }
   fade(delay) {
@@ -37,6 +48,51 @@ class Contact extends Component {
       return {'opacity': `${this.state.componentOpacity}`, 'transition': 'opacity 1.5s', 'transitionDelay': `${delay}s`}
     } else {
       return {'opacity': '0', 'transition': 'opacity .5s', 'transitionDelay': '0s'}
+    }
+  }
+  checkSendStatus() {
+    switch(this.state.status) {
+      case 'sending':
+      return(
+        <div className="statusContainer">
+          <div className="statusInfo">
+            <div className="statusContent">SENDING</div>
+            <div className="progressContainer">
+              <div className="progressBar" style={{'animationDelay': '.2s'}}></div>
+              <div className="progressBar" style={{'animationDelay': '.6s'}}></div>
+              <div className="progressBar" style={{'animationDelay': '1s'}}></div>
+            </div>
+          </div>
+        </div>
+      )
+
+      case 'sent':
+      return(
+        <div className="statusContainer">
+          <div className="statusInfo">
+            <div className="sendConfirm">Thank you for your message!</div>
+          </div>
+        </div>
+      )
+
+      default:
+      return(<form action="javascript:void(0);" onSubmit={this.submitForm}>
+        <div className="nameEmailContainer">
+          <div>
+            <input id="name"type="text" autoCapitalize="words" required onChange={(e) => this.updateField('name', e)} />
+            <label htmlFor="name">Name/Company</label>
+          </div>
+          <div>
+            <input id="email" type="email" required onChange={(e) => this.updateField('email', e)} />
+            <label htmlFor="email">Email</label>
+          </div>
+        </div>
+        <div className="textContainer">
+          <textarea id="message" type="text" required onChange={(e) => this.updateField('text', e)}></textarea>
+          <label htmlFor="message">Message</label>
+        </div>
+        <button type="submit">Send</button>
+      </form>)
     }
   }
   componentDidMount() {
@@ -51,23 +107,7 @@ class Contact extends Component {
   render() {
     return(
       <div className='lastOut contact' style={this.fade(.1)}>
-        <form action="javascript:void(0);" onSubmit={this.submitForm}>
-          <div className="nameEmailContainer">
-            <div>
-              <input id="name"type="text" autoCapitalize="words" required onChange={(e) => this.updateField('name', e)} />
-              <label htmlFor="name">Name/Company</label>
-            </div>
-            <div>
-              <input id="email" type="email" required onChange={(e) => this.updateField('email', e)} />
-              <label htmlFor="email">Email</label>
-            </div>
-          </div>
-          <div className="textContainer">
-            <textarea id="message" type="text" required onChange={(e) => this.updateField('text', e)}></textarea>
-            <label htmlFor="message">Message</label>
-          </div>
-          <button type="submit"></button>
-        </form>
+        {this.checkSendStatus()}
       </div>
     )
   }
