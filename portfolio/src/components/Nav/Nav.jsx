586e34c8ef6f651d5711e4df528685c6e219ec4c
Matt Bedell <matt@bedell.co>
2017-02-06 15:05:35 -0500

    styling tweaks

--- portfolio/src/components/Nav/Nav.jsx
+++ portfolio/src/components/Nav/Nav.jsx
@@ -12,9 +12,9 @@ import './Nav.css';
 const Nav = (props) => (
   <div className="navBar">
     <div className="navButton" onClick={() => props.handleNavClick('about')}>About</div>
-    <div>|</div>
+    <div className="navDivider">|</div>
     <div className="navButton" onClick={() => props.handleNavClick('project')}>My Work</div>
-    <div>|</div>
+    <div className="navDivider">|</div>
     <div className="navButton" onClick={() => props.handleNavClick('contact')}>Contact</div>
   </div>
 )
